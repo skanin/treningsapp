12be274e40baafe6a11c7dfb1f38669363383538
Sander Lindberg <Sander.b.lindberg@gmail.com>
2019-03-11 12:47:32 +0100

    First commit

---
+++ src/DBConn.java
@@ -0,0 +1,32 @@
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.util.Properties;
+
+public class DBConn {
+    protected Connection conn;
+
+    public DBConn() {
+    }
+
+    public void connect() {
+        try {
+            // db parameters
+            String url       = "jdbc:mysql://mysql.stud.ntnu.no:3306/sanderbl_dbproject";
+            String user      = "sanderbl_dbproject";
+            String password  = "1234";
+
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected sucessfully");
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to connect", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        DBConn conn = new DBConn();
+
+        conn.connect();
+
+    }
+}
